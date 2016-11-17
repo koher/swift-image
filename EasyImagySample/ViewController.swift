341421abe35fabbfd51a2a3f43e886ff2fe643a6
Yuta Koshizawa <koher@koherent.org>
2016-11-17 10:48:30 +0900

    Remove unnecessary `?.`

--- EasyImagySample/ViewController.swift
+++ EasyImagySample/ViewController.swift
@@ -5,80 +5,80 @@ class ViewController: UIViewController {
 	@IBOutlet fileprivate var imageView: UIImageView!
 
 	@IBAction fileprivate func onPressActionButton(_ sender: UIButton) {
-		let imageView = self.imageView
+        let imageView: UIImageView! = self.imageView
 		
 		let controller = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
 
 		controller.addAction(UIAlertAction(title: "Reset", style: .default) { action in
-			imageView?.image = UIImage(named: "Image.jpg")
+			imageView.image = UIImage(named: "Image.jpg")
 			sender.isEnabled = true
 		})
 
 		controller.addAction(UIAlertAction(title: "Flip Horizontally", style: .default) { action in
-			imageView?.image = Image<RGBA>(uiImage: (imageView?.image!)!)!.flipX().uiImage
+			imageView.image = Image<RGBA>(uiImage: imageView.image!)!.flipX().uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Flip Vertically", style: .default) { action in
-			imageView?.image = Image<RGBA>(uiImage: (imageView?.image!)!)!.flipY().uiImage
+			imageView.image = Image<RGBA>(uiImage: imageView.image!)!.flipY().uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Rotate clockwise", style: .default) { action in
-			imageView?.image = Image<RGBA>(uiImage: (imageView?.image!)!)!.rotate().uiImage
+			imageView.image = Image<RGBA>(uiImage: imageView.image!)!.rotate().uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Rotate counterclockwise", style: .default) { action in
-			imageView?.image = Image<RGBA>(uiImage: (imageView?.image!)!)!.rotate(-1).uiImage
+			imageView.image = Image<RGBA>(uiImage: imageView.image!)!.rotate(-1).uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Rotate by 180 degrees", style: .default) { action in
-			imageView?.image = Image<RGBA>(uiImage: (imageView?.image!)!)!.rotate(2).uiImage
+			imageView.image = Image<RGBA>(uiImage: imageView.image!)!.rotate(2).uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Resize", style: .default) { action in
-			imageView?.image = Image<RGBA>(uiImage: (imageView?.image!)!)!.resize(width: 100, height: 100).uiImage
+			imageView.image = Image<RGBA>(uiImage: imageView.image!)!.resize(width: 100, height: 100).uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Crop", style: .default) { action in
-			imageView?.image = Image<RGBA>(Image(uiImage: (imageView?.image!)!)![0..<100, 0..<100]).uiImage
+			imageView.image = Image<RGBA>(Image(uiImage: imageView.image!)![0..<100, 0..<100]).uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Grayscale", style: .default) { action in
-			imageView?.image = Image<RGBA>(uiImage: (imageView?.image!)!)!.map { (pixel: RGBA) -> RGBA in
+			imageView.image = Image<RGBA>(uiImage: imageView.image!)!.map { (pixel: RGBA) -> RGBA in
 				RGBA(gray: pixel.gray)
 			}.uiImage
 			sender.isEnabled = true
 		})
 
 		controller.addAction(UIAlertAction(title: "Binarize", style: .default) { action in
-			imageView?.image = Image<RGBA>(uiImage: (imageView?.image!)!)!.map { (pixel: RGBA) -> RGBA in
+			imageView.image = Image<RGBA>(uiImage: imageView.image!)!.map { (pixel: RGBA) -> RGBA in
 				pixel.gray < 128 ? RGBA.black : RGBA.white
 			}.uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Binarize (Auto threshold)", style: .default) { action in
-			let image = Image<RGBA>(uiImage: (imageView?.image!)!)!
+			let image = Image<RGBA>(uiImage: imageView.image!)!
 			let threshold = UInt8(image.reduce(0) { $0 + $1.grayInt } / image.count) // mean of gray values of all pixels
-			imageView?.image = image.map { $0.gray < threshold ? .black : .white }.uiImage
+			imageView.image = image.map { $0.gray < threshold ? .black : .white }.uiImage
 			sender.isEnabled = true
 		})
 
 		controller.addAction(UIAlertAction(title: "Mean filter", style: .default) { action in
-			let image = Image<RGBA>(uiImage: (imageView?.image!)!)!
+			let image = Image<RGBA>(uiImage: imageView.image!)!
             let kernel = Image<Float>(width: 3, height: 3, pixel: 1.0 / 9.0)
-			imageView?.image = image.convoluted(kernel).uiImage
+			imageView.image = image.convoluted(kernel).uiImage
 			sender.isEnabled = true
 		})
 		
 		controller.addAction(UIAlertAction(title: "Gaussian filter", style: .default) { action in
-			let image = Image<RGBA>(uiImage: (imageView?.image!)!)!
+			let image = Image<RGBA>(uiImage: imageView.image!)!
             let kernel = Image<Int>(width: 5, height: 5, pixels: [
                 1,  4,  6,  4, 1,
                 4, 16, 24, 16, 4,
@@ -86,7 +86,7 @@ class ViewController: UIViewController {
                 4, 16, 24, 16, 4,
                 1,  4,  6,  4, 1,
             ]).map { Float($0) / 256.0 }
-			imageView?.image = image.convoluted(kernel).uiImage
+			imageView.image = image.convoluted(kernel).uiImage
 			sender.isEnabled = true
 		})
 		
