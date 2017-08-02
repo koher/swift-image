3d9f6ff28165679cea93bd8e5c690ddb855a318d
Yuta Koshizawa <koher@koherent.org>
2017-08-02 17:27:36 +0900

    Add tests to `testCgImage` for `Image<UInt8>`

--- EasyImagyTests/ImageCoreGraphicsTest.swift
+++ EasyImagyTests/ImageCoreGraphicsTest.swift
@@ -12,12 +12,27 @@ import EasyImagy
                     RGBA(red: 253, green: 254, blue: 255, alpha: 255),
                     RGBA(red: 10, green: 20, blue: 30, alpha: 102),
                     RGBA(red: 10, green: 20, blue: 30, alpha: 51),
-                    ])
+                ])
                 let cgImage = image.cgImage
                 XCTAssertEqual(cgImage.width, image.width)
                 XCTAssertEqual(cgImage.height, image.height)
                 
                 let restored = Image<RGBA>(cgImage: cgImage)
+                XCTAssertEqual(restored.width, image.width)
+                XCTAssertEqual(restored.height, image.height)
+                XCTAssertEqual(restored[0, 0], image[0, 0])
+                XCTAssertEqual(restored[1, 0], image[1, 0])
+                XCTAssertEqual(restored[0, 1], image[0, 1])
+                XCTAssertEqual(restored[1, 1], image[1, 1])
+            }
+            
+            do {
+                let image = Image<UInt8>(width: 2, height: 2, pixels: [0, 1, 127, 255])
+                let cgImage = image.cgImage
+                XCTAssertEqual(cgImage.width, image.width)
+                XCTAssertEqual(cgImage.height, image.height)
+                
+                let restored = Image<UInt8>(cgImage: cgImage)
                 XCTAssertEqual(restored.width, image.width)
                 XCTAssertEqual(restored.height, image.height)
                 XCTAssertEqual(restored[0, 0], image[0, 0])
