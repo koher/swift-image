303252097f0bbcb6a205b38c0a48737d68b40bd4
Yuta Koshizawa <koher@koherent.org>
2017-10-26 14:03:06 +0900

    Remove `clamp` from summable casting initializers

--- Sources/EasyImagy/Summable.swift
+++ Sources/EasyImagy/Summable.swift
@@ -27,28 +27,28 @@ internal func *<T: _Multipliable>(lhs: RGBA<T>, rhs: T) -> RGBA<T> {
 extension RGBA where Channel == UInt8 {
     internal init(summableI: RGBA<Int>) {
         self.init(
-            red: UInt8(clamp(summableI.red, lower: 0, upper: Int(UInt8.max))),
-            green: UInt8(clamp(summableI.green, lower: 0, upper: Int(UInt8.max))),
-            blue: UInt8(clamp(summableI.blue, lower: 0, upper: Int(UInt8.max))),
-            alpha: UInt8(clamp(summableI.alpha, lower: 0, upper: Int(UInt8.max)))
+            red: UInt8(summableI.red),
+            green: UInt8(summableI.green),
+            blue: UInt8(summableI.blue),
+            alpha: UInt8(summableI.alpha)
         )
     }
 
     internal init(summableF: RGBA<Float>) {
         self.init(
-            red: UInt8(clamp(summableF.red, lower: 0, upper: Float(UInt8.max))),
-            green: UInt8(clamp(summableF.green, lower: 0, upper: Float(UInt8.max))),
-            blue: UInt8(clamp(summableF.blue, lower: 0, upper: Float(UInt8.max))),
-            alpha: UInt8(clamp(summableF.alpha, lower: 0, upper: Float(UInt8.max)))
+            red: UInt8(summableF.red),
+            green: UInt8(summableF.green),
+            blue: UInt8(summableF.blue),
+            alpha: UInt8(summableF.alpha)
         )
     }
 
     internal init(summableD: RGBA<Double>) {
         self.init(
-            red: UInt8(clamp(summableD.red, lower: 0, upper: Double(UInt8.max))),
-            green: UInt8(clamp(summableD.green, lower: 0, upper: Double(UInt8.max))),
-            blue: UInt8(clamp(summableD.blue, lower: 0, upper: Double(UInt8.max))),
-            alpha: UInt8(clamp(summableD.alpha, lower: 0, upper: Double(UInt8.max)))
+            red: UInt8(summableD.red),
+            green: UInt8(summableD.green),
+            blue: UInt8(summableD.blue),
+            alpha: UInt8(summableD.alpha)
         )
     }
     
@@ -92,28 +92,28 @@ extension RGBA where Channel == UInt8 {
 extension RGBA where Channel == UInt16 {
     internal init(summableI: RGBA<Int>) {
         self.init(
-            red: UInt16(clamp(summableI.red, lower: 0, upper: Int(UInt16.max))),
-            green: UInt16(clamp(summableI.green, lower: 0, upper: Int(UInt16.max))),
-            blue: UInt16(clamp(summableI.blue, lower: 0, upper: Int(UInt16.max))),
-            alpha: UInt16(clamp(summableI.alpha, lower: 0, upper: Int(UInt16.max)))
+            red: UInt16(summableI.red),
+            green: UInt16(summableI.green),
+            blue: UInt16(summableI.blue),
+            alpha: UInt16(summableI.alpha)
         )
     }
 
     internal init(summableF: RGBA<Float>) {
         self.init(
-            red: UInt16(clamp(summableF.red, lower: 0, upper: Float(UInt16.max))),
-            green: UInt16(clamp(summableF.green, lower: 0, upper: Float(UInt16.max))),
-            blue: UInt16(clamp(summableF.blue, lower: 0, upper: Float(UInt16.max))),
-            alpha: UInt16(clamp(summableF.alpha, lower: 0, upper: Float(UInt16.max)))
+            red: UInt16(summableF.red),
+            green: UInt16(summableF.green),
+            blue: UInt16(summableF.blue),
+            alpha: UInt16(summableF.alpha)
         )
     }
 
     internal init(summableD: RGBA<Double>) {
         self.init(
-            red: UInt16(clamp(summableD.red, lower: 0, upper: Double(UInt16.max))),
-            green: UInt16(clamp(summableD.green, lower: 0, upper: Double(UInt16.max))),
-            blue: UInt16(clamp(summableD.blue, lower: 0, upper: Double(UInt16.max))),
-            alpha: UInt16(clamp(summableD.alpha, lower: 0, upper: Double(UInt16.max)))
+            red: UInt16(summableD.red),
+            green: UInt16(summableD.green),
+            blue: UInt16(summableD.blue),
+            alpha: UInt16(summableD.alpha)
         )
     }
     
@@ -157,28 +157,28 @@ extension RGBA where Channel == UInt16 {
 extension RGBA where Channel == UInt32 {
     internal init(summableI: RGBA<Int64>) {
         self.init(
-            red: UInt32(clamp(summableI.red, lower: 0, upper: Int64(UInt32.max))),
-            green: UInt32(clamp(summableI.green, lower: 0, upper: Int64(UInt32.max))),
-            blue: UInt32(clamp(summableI.blue, lower: 0, upper: Int64(UInt32.max))),
-            alpha: UInt32(clamp(summableI.alpha, lower: 0, upper: Int64(UInt32.max)))
+            red: UInt32(summableI.red),
+            green: UInt32(summableI.green),
+            blue: UInt32(summableI.blue),
+            alpha: UInt32(summableI.alpha)
         )
     }
 
     internal init(summableF: RGBA<Float>) {
         self.init(
-            red: UInt32(clamp(summableF.red, lower: 0, upper: Float(UInt32.max))),
-            green: UInt32(clamp(summableF.green, lower: 0, upper: Float(UInt32.max))),
-            blue: UInt32(clamp(summableF.blue, lower: 0, upper: Float(UInt32.max))),
-            alpha: UInt32(clamp(summableF.alpha, lower: 0, upper: Float(UInt32.max)))
+            red: UInt32(summableF.red),
+            green: UInt32(summableF.green),
+            blue: UInt32(summableF.blue),
+            alpha: UInt32(summableF.alpha)
         )
     }
 
     internal init(summableD: RGBA<Double>) {
         self.init(
-            red: UInt32(clamp(summableD.red, lower: 0, upper: Double(UInt32.max))),
-            green: UInt32(clamp(summableD.green, lower: 0, upper: Double(UInt32.max))),
-            blue: UInt32(clamp(summableD.blue, lower: 0, upper: Double(UInt32.max))),
-            alpha: UInt32(clamp(summableD.alpha, lower: 0, upper: Double(UInt32.max)))
+            red: UInt32(summableD.red),
+            green: UInt32(summableD.green),
+            blue: UInt32(summableD.blue),
+            alpha: UInt32(summableD.alpha)
         )
     }
     
@@ -222,28 +222,28 @@ extension RGBA where Channel == UInt32 {
 extension RGBA where Channel == Int {
     internal init(summableI: RGBA<Int>) {
         self.init(
-            red: Int(clamp(summableI.red, lower: 0, upper: Int(255))),
-            green: Int(clamp(summableI.green, lower: 0, upper: Int(255))),
-            blue: Int(clamp(summableI.blue, lower: 0, upper: Int(255))),
-            alpha: Int(clamp(summableI.alpha, lower: 0, upper: Int(255)))
+            red: Int(summableI.red),
+            green: Int(summableI.green),
+            blue: Int(summableI.blue),
+            alpha: Int(summableI.alpha)
         )
     }
 
     internal init(summableF: RGBA<Float>) {
         self.init(
-            red: Int(clamp(summableF.red, lower: 0, upper: Float(255))),
-            green: Int(clamp(summableF.green, lower: 0, upper: Float(255))),
-            blue: Int(clamp(summableF.blue, lower: 0, upper: Float(255))),
-            alpha: Int(clamp(summableF.alpha, lower: 0, upper: Float(255)))
+            red: Int(summableF.red),
+            green: Int(summableF.green),
+            blue: Int(summableF.blue),
+            alpha: Int(summableF.alpha)
         )
     }
 
     internal init(summableD: RGBA<Double>) {
         self.init(
-            red: Int(clamp(summableD.red, lower: 0, upper: Double(255))),
-            green: Int(clamp(summableD.green, lower: 0, upper: Double(255))),
-            blue: Int(clamp(summableD.blue, lower: 0, upper: Double(255))),
-            alpha: Int(clamp(summableD.alpha, lower: 0, upper: Double(255)))
+            red: Int(summableD.red),
+            green: Int(summableD.green),
+            blue: Int(summableD.blue),
+            alpha: Int(summableD.alpha)
         )
     }
     
@@ -287,28 +287,28 @@ extension RGBA where Channel == Int {
 extension RGBA where Channel == Float {
     internal init(summableI: RGBA<Float>) {
         self.init(
-            red: Float(clamp(summableI.red, lower: 0, upper: Float(1))),
-            green: Float(clamp(summableI.green, lower: 0, upper: Float(1))),
-            blue: Float(clamp(summableI.blue, lower: 0, upper: Float(1))),
-            alpha: Float(clamp(summableI.alpha, lower: 0, upper: Float(1)))
+            red: Float(summableI.red),
+            green: Float(summableI.green),
+            blue: Float(summableI.blue),
+            alpha: Float(summableI.alpha)
         )
     }
 
     internal init(summableF: RGBA<Float>) {
         self.init(
-            red: Float(clamp(summableF.red, lower: 0, upper: Float(1))),
-            green: Float(clamp(summableF.green, lower: 0, upper: Float(1))),
-            blue: Float(clamp(summableF.blue, lower: 0, upper: Float(1))),
-            alpha: Float(clamp(summableF.alpha, lower: 0, upper: Float(1)))
+            red: Float(summableF.red),
+            green: Float(summableF.green),
+            blue: Float(summableF.blue),
+            alpha: Float(summableF.alpha)
         )
     }
 
     internal init(summableD: RGBA<Double>) {
         self.init(
-            red: Float(clamp(summableD.red, lower: 0, upper: Double(1))),
-            green: Float(clamp(summableD.green, lower: 0, upper: Double(1))),
-            blue: Float(clamp(summableD.blue, lower: 0, upper: Double(1))),
-            alpha: Float(clamp(summableD.alpha, lower: 0, upper: Double(1)))
+            red: Float(summableD.red),
+            green: Float(summableD.green),
+            blue: Float(summableD.blue),
+            alpha: Float(summableD.alpha)
         )
     }
     
@@ -352,28 +352,28 @@ extension RGBA where Channel == Float {
 extension RGBA where Channel == Double {
     internal init(summableI: RGBA<Double>) {
         self.init(
-            red: Double(clamp(summableI.red, lower: 0, upper: Double(1))),
-            green: Double(clamp(summableI.green, lower: 0, upper: Double(1))),
-            blue: Double(clamp(summableI.blue, lower: 0, upper: Double(1))),
-            alpha: Double(clamp(summableI.alpha, lower: 0, upper: Double(1)))
+            red: Double(summableI.red),
+            green: Double(summableI.green),
+            blue: Double(summableI.blue),
+            alpha: Double(summableI.alpha)
         )
     }
 
     internal init(summableF: RGBA<Double>) {
         self.init(
-            red: Double(clamp(summableF.red, lower: 0, upper: Double(1))),
-            green: Double(clamp(summableF.green, lower: 0, upper: Double(1))),
-            blue: Double(clamp(summableF.blue, lower: 0, upper: Double(1))),
-            alpha: Double(clamp(summableF.alpha, lower: 0, upper: Double(1)))
+            red: Double(summableF.red),
+            green: Double(summableF.green),
+            blue: Double(summableF.blue),
+            alpha: Double(summableF.alpha)
         )
     }
 
     internal init(summableD: RGBA<Double>) {
         self.init(
-            red: Double(clamp(summableD.red, lower: 0, upper: Double(1))),
-            green: Double(clamp(summableD.green, lower: 0, upper: Double(1))),
-            blue: Double(clamp(summableD.blue, lower: 0, upper: Double(1))),
-            alpha: Double(clamp(summableD.alpha, lower: 0, upper: Double(1)))
+            red: Double(summableD.red),
+            green: Double(summableD.green),
+            blue: Double(summableD.blue),
+            alpha: Double(summableD.alpha)
         )
     }
     
@@ -416,15 +416,15 @@ extension RGBA where Channel == Double {
 
 extension UInt8 {
     internal init(summableI: Int) {
-        self = UInt8(clamp(summableI, lower: 0, upper: Int(UInt8.max)))
+        self = UInt8(summableI)
     }
 
     internal init(summableF: Float) {
-        self = UInt8(clamp(summableF, lower: 0, upper: Float(UInt8.max)))
+        self = UInt8(summableF)
     }
 
     internal init(summableD: Double) {
-        self = UInt8(clamp(summableD, lower: 0, upper: Double(UInt8.max)))
+        self = UInt8(summableD)
     }
     
     internal var summableI: Int {
@@ -469,15 +469,15 @@ extension UInt8 {
 
 extension UInt16 {
     internal init(summableI: Int) {
-        self = UInt16(clamp(summableI, lower: 0, upper: Int(UInt16.max)))
+        self = UInt16(summableI)
     }
 
     internal init(summableF: Float) {
-        self = UInt16(clamp(summableF, lower: 0, upper: Float(UInt16.max)))
+        self = UInt16(summableF)
     }
 
     internal init(summableD: Double) {
-        self = UInt16(clamp(summableD, lower: 0, upper: Double(UInt16.max)))
+        self = UInt16(summableD)
     }
     
     internal var summableI: Int {
@@ -522,15 +522,15 @@ extension UInt16 {
 
 extension UInt32 {
     internal init(summableI: Int64) {
-        self = UInt32(clamp(summableI, lower: 0, upper: Int64(UInt32.max)))
+        self = UInt32(summableI)
     }
 
     internal init(summableF: Float) {
-        self = UInt32(clamp(summableF, lower: 0, upper: Float(UInt32.max)))
+        self = UInt32(summableF)
     }
 
     internal init(summableD: Double) {
-        self = UInt32(clamp(summableD, lower: 0, upper: Double(UInt32.max)))
+        self = UInt32(summableD)
     }
     
     internal var summableI: Int64 {
@@ -575,15 +575,15 @@ extension UInt32 {
 
 extension Int {
     internal init(summableI: Int) {
-        self = Int(clamp(summableI, lower: 0, upper: Int(255)))
+        self = Int(summableI)
     }
 
     internal init(summableF: Float) {
-        self = Int(clamp(summableF, lower: 0, upper: Float(255)))
+        self = Int(summableF)
     }
 
     internal init(summableD: Double) {
-        self = Int(clamp(summableD, lower: 0, upper: Double(255)))
+        self = Int(summableD)
     }
     
     internal var summableI: Int {
@@ -628,15 +628,15 @@ extension Int {
 
 extension Float {
     internal init(summableI: Float) {
-        self = Float(clamp(summableI, lower: 0, upper: Float(1)))
+        self = Float(summableI)
     }
 
     internal init(summableF: Float) {
-        self = Float(clamp(summableF, lower: 0, upper: Float(1)))
+        self = Float(summableF)
     }
 
     internal init(summableD: Double) {
-        self = Float(clamp(summableD, lower: 0, upper: Double(1)))
+        self = Float(summableD)
     }
     
     internal var summableI: Float {
@@ -681,15 +681,15 @@ extension Float {
 
 extension Double {
     internal init(summableI: Double) {
-        self = Double(clamp(summableI, lower: 0, upper: Double(1)))
+        self = Double(summableI)
     }
 
     internal init(summableF: Double) {
-        self = Double(clamp(summableF, lower: 0, upper: Double(1)))
+        self = Double(summableF)
     }
 
     internal init(summableD: Double) {
-        self = Double(clamp(summableD, lower: 0, upper: Double(1)))
+        self = Double(summableD)
     }
     
     internal var summableI: Double {
