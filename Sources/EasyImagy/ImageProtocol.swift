b1ff7adb98af8de42ab6b3403b861c2aec3f39ff
Yuta Koshizawa <koher@koherent.org>
2017-12-01 23:39:15 +0900

    Add ranged `subscript`s to `ImageProtocol`

--- Sources/EasyImagy/ImageProtocol.swift
+++ Sources/EasyImagy/ImageProtocol.swift
@@ -4,6 +4,12 @@ public protocol ImageProtocol : Sequence {
     var width: Int { get }
     var height: Int { get }
     subscript(x: Int, y: Int) -> Pixel { get set }
+    
+    subscript(xRange: CountableRange<Int>, yRange: CountableRange<Int>) -> ImageSlice<Pixel> { get }
+    subscript<R1: RangeExpression, R2: RangeExpression>(xRange: R1, yRange: R2) -> ImageSlice<Pixel> where R1.Bound == Int, R2.Bound == Int { get }
+    subscript<R1: RangeExpression>(xRange: R1, yRange: UnboundedRange) -> ImageSlice<Pixel> where R1.Bound == Int { get }
+    subscript<R2: RangeExpression>(xRange: UnboundedRange, yRange: R2) -> ImageSlice<Pixel> where R2.Bound == Int { get }
+    subscript(xRange: UnboundedRange, yRange: UnboundedRange) -> ImageSlice<Pixel> { get }
 }
 
 extension ImageProtocol {
