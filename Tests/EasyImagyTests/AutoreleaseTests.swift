6252f596ff091eeb461f73f0dd7a769880a6e9b6
Yuta Koshizawa <koher@koherent.org>
2019-02-11 16:31:34 +0900

    Add AutoreleaseTests.swift

---
+++ Tests/EasyImagyTests/AutoreleaseTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+import EasyImagy
+
+class AutoreleaseTests: XCTestCase {
+    func testPNGData() {
+        let image = Image<UInt8>(width: 100, height: 100, pixel: 42)
+        
+        for _ in 0..<100 {
+            let _ = image.data(using: .png)
+        }
+        
+        XCTAssertTrue(true) // Break here and check if `CGImage` instances are released.
+    }
+    
+    func testJPEGData() {
+        let image = Image<UInt8>(width: 100, height: 100, pixel: 42)
+        
+        for _ in 0..<100 {
+            let _ = image.data(using: .jpeg(compressionQuality: 0.8))
+        }
+        
+        XCTAssertTrue(true) // Break here and check if `CGImage` instances are released.
+    }
+}
