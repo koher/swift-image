9257067195ba1a25b3a92126dca69f8ae9e97011
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-09-05 15:43:13 +0200

    Added support for Swift Tools version 5.2

--- Package.swift
+++ Package.swift
@@ -1,15 +1,13 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftImage",
+    name: "swift-image",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftImage",
-            targets: ["SwiftImage"]),
+        .library(name: "SwiftImage", targets: ["SwiftImage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +16,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftImage",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftImageTests",
-            dependencies: ["SwiftImage"]),
+        .target(name: "SwiftImage"),
+        .testTarget(name: "SwiftImageTests", dependencies: [
+            .target(name: "SwiftImage")
+        ]),
     ]
 )
