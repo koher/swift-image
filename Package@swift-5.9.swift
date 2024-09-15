2028feef86e3a4a95d4421df0b71a25c308889e4
Yuta Koshizawa <koher@koherent.org>
2024-09-15 14:39:31 +0900

    Add Package@swift-5.9.swift

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftImage",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftImage",
+            targets: ["SwiftImage"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftImage",
+            dependencies: [],
+            exclude: [
+                "Convolution.swift.gyb",
+                "RGBAOperators.swift.gyb",
+                "NumericPixel.swift.gyb",
+                "ImageOperators.swift.gyb"
+            ],
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]),
+        .testTarget(
+            name: "SwiftImageTests",
+            dependencies: ["SwiftImage"],
+            resources: [
+                .process("Test2x1.png"),
+                .process("Test2x2.png"),
+                .process("Test4x4.png"),
+            ]),
+    ]
+)
